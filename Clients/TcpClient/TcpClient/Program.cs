bcfd6e25fba6ba9b6e8f6199c38db61af6b5be6c
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2023-03-06 22:39:51 +0200

    Added raw tcp client and server

---
+++ Clients/TcpClient/TcpClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+TcpClient clientTcp = new TcpClient();
+clientTcp.Connect(IPAddress.Parse("127.0.0.1"), 8000);
+NetworkStream streamTcp = clientTcp.GetStream();
+byte[] buffer = new byte[8];
+buffer = Encoding.UTF8.GetBytes("Hello");
+streamTcp.Write(buffer, 0, buffer.Length);
+streamTcp.Close();
+clientTcp.Close();
+Console.WriteLine("Передача выполнена. Нажмите любую клавишу для завершения...");
+Console.ReadKey();
---
+++ Servers/TcpServer/TcpServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+TcpListener tcpServer = new TcpListener(IPAddress.Parse("127.0.0.1"), 8000);
+tcpServer.Start();
+Console.WriteLine("Ожидание подключения...");
+TcpClient tcpClient = tcpServer.AcceptTcpClient();
+Console.WriteLine("Подключение установлено, входное сообщение:");
+byte[] buffer = new byte[8];
+NetworkStream streamTcp = tcpClient.GetStream();
+streamTcp.Read(buffer, 0, buffer.Length);
+Console.WriteLine(Encoding.UTF8.GetString(buffer));
+streamTcp.Close();
+tcpClient.Close();
+tcpServer.Stop();
+Console.WriteLine("Нажмите любую клавишу для завершения...");
+Console.ReadKey();
+
