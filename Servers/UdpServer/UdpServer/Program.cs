e0f14ae28d758f6d7e6b1ea8973acb5e4a57cbfc
DimaMykhnevych <dmitriy.mykhnevych@gmail.com>
2023-03-07 08:29:54 +0200

    Added raw UDP server and client

---
+++ Servers/UdpServer/UdpServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+byte[] buffer = new byte[8];
+IPEndPoint ipEndpoint = new (IPAddress.Any, 8000);
+Socket serverSock = new (AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+serverSock.Bind(ipEndpoint);
+Console.WriteLine("Waitinf for UDP protocol connection ...");
+serverSock.Receive(buffer);
+
+Console.WriteLine("Sending data UDP:");
+Console.WriteLine(Encoding.UTF8.GetString(buffer));
+serverSock.Close();
+Console.WriteLine("Press any key to finish...");
+Console.ReadKey();
